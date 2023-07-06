cc0b4c783118eed63a3bd19d719bbeb6fd8574d9
davide <d.cannerozzi@gmail.com>
2023-07-06 15:19:40 +0200

    started implementing hamburger menu

--- src/components/header/menu/Menu.tsx
+++ src/components/header/menu/Menu.tsx
@@ -1,17 +1,47 @@
-import Link from "next/link"
+import { faBurger } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+import { useState } from 'react';
 
 function Menu() {
-    return(
-        <div>
-            <ul className="flex space-x-2">
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About Us</Link></li>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/">Home</Link></li>
-            </ul>
-        </div>
-    )
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={handleMenu}
+        className="md:hidden"
+      >
+        <FontAwesomeIcon icon={faBurger} />
+      </button>
+
+      <ul
+        className={`flex space-x-2 font-bold text-sm ${
+          isOpen ? 'block ' : 'hidden md:flex'
+        }`}
+      >
+        <li>
+          <Link href="/">Find Talent</Link>
+        </li>
+        <li>
+          <Link href="/about">For Designers</Link>
+        </li>
+        <li>
+          <Link href="/">Inspiration</Link>
+        </li>
+        <li>
+          <Link href="/">Learn Design</Link>
+        </li>
+        <li className="text-pink-400">
+          <Link href="/">Go Pro</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
--- src/components/header/searchBar/SearchBar.tsx
+++ src/components/header/searchBar/SearchBar.tsx
@@ -1,11 +1,24 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function SearchBar() {
-    return(
-        <div>
-            <form action="" className="border border-black">
-                <input type="text" name="" id="" />
-            </form>
+  return (
+    <div className="relative">
+      <form action="">
+        <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
         </div>
-    )
+
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Search"
+          className="p-4 rounded-3xl border-y-gray-300"
+        />
+      </form>
+    </div>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
